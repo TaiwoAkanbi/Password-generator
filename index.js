9f2f21ba3aba8d40dea674c210351e5cdd00edf7
TaiwoAkanbi <akanbitaiwoadetoun@gmail.com>
2026-03-02 11:11:21 +0100

    updated files
    I updated all the files

---
+++ index.js
@@ -0,0 +1,115 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let btnElement = document.querySelector(".btn");
+let button1 = document.querySelector(".btn1");
+let button2 = document.querySelector(".btn2");
+
+function getRandomCharacter() {
+  let randomIndex = Math.floor(Math.random() * characters.length);
+  return characters[randomIndex];
+}
+function generate() {
+  let length = 7;
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += getRandomCharacter();
+  }
+  return password;
+}
+
+btnElement.addEventListener("click", function () {
+  button1.textContent = generate();
+  button2.textContent = generate();
+});
